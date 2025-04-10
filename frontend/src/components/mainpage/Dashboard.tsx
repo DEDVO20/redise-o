4a6be80e34ed726d9ed35b78db919f32cbad3aeb
AlexCalderon-c <estivenalexandercalderonsossa@gmail.com>
2025-04-09 21:28:05 -0500

    Mejora del dashboard, se está trabajando en el diseño responsive

--- frontend/src/components/mainpage/Dashboard.tsx
+++ frontend/src/components/mainpage/Dashboard.tsx
@@ -11,7 +11,7 @@ const taskOptions = [
 function Dashboard() {
   return (
     <>
-      <section className='grid grid-cols-1 md:grid-cols-2 grid-rows-[130px_minmax(400px,_auto)] gap-7 m-5 h-[calc(100vh-2.5rem)]'>
+      <section className='grid grid-cols-2 grid-rows-[130px_minmax(400px,_auto)] gap-7 m-5 h-[calc(100vh-2.5rem)]'>
         <div className="shadow-xl flex flex-col items-start justify-evenly p-5 bg-white rounded-lg">
           <p className="text-gray-600">Su carrera es: </p>
           <h2 className='text-2xl md:text-lg lg:text-2xl bg-white font-bold'>
@@ -19,8 +19,8 @@ function Dashboard() {
           </h2>
         </div>
         <div className="shadow-xl bg-white rounded-lg p-6"></div>
-        <div className="grid grid-cols-[480px_1fr] col-span-1 md:col-span-2 shadow-xl bg-white rounded-lg p-12">
-          <div className="w-full h-full flex flex-col items-start justify-center gap-5">
+        <div className="col-span-2 w-full flex flex-row flex-wrap xl:flex-nowrap shadow-xl bg-white rounded-lg p-12">
+          <div className="w-300 flex flex-col items-start justify-center gap-5">
             <h2 className='text-xl sm:text-2xl md:text-2xl lg:text-5xl bg-white font-bold' >
               Bienvenido a el <span className="text-[#0072C6]">Portal de Notas</span>
             </h2>
@@ -30,7 +30,7 @@ function Dashboard() {
               con rápidez
             </p>
           </div>
-          <div className="flex flex-col items-start justify-start gap-8 p-6">
+          <div className="w-full flex flex-col items-start justify-start gap-8 p-6">
             <div className="w-full">
               <h2 className="text-2xl font-bold mb-2">Últimas tareas realizadas</h2>
               <div className="border-l-2 border-gray-200 mb-4"></div>
--- frontend/src/components/mainpage/TaskItems.tsx
+++ frontend/src/components/mainpage/TaskItems.tsx
@@ -6,9 +6,9 @@ interface TaskItemProps {
 
 function TaskItems({ title, buttonText = 'Realizar', imageUrl = ''}: TaskItemProps) {
   return (
-    <div className="flex items-center justify-between p-4 border-l-6 border-[#0072C6] shadow-lg">
+    <div className="flex items-center justify-between max-w-200 max-h-100 p-4 border-l-6 border-[#0072C6] shadow-lg">
       <div className="flex items-center gap-4">
-        <div className="w-12 h-12 bg-gray-200 rounded-full">
+        <div className="max-w-12 max-h-12 bg-gray-200 rounded-full">
           {imageUrl && <img src={imageUrl} alt="Task" className="w-full h-full rounded-full" />}
         </div>
         <span className="font-medium">{title}</span>
